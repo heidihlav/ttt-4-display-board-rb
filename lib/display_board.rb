dbde2f3e224a558f310feb180214e8bc13339a77
Heidi Hlavinka <hchlav@gmail.com>
2021-07-25 15:14:33 -0500

    commit tree 10e5084116bb88dc60e364bb83f9c0a810f6ec7e

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+require 'pry'
+
+board = [" "," "," ","X "," "," "," "," "," "]
+
+def display_board(board)
+    puts " #{board [0]} " "|" " #{board [1]} " "|" " #{board [2]} "
+    puts "-----------" 
+    puts " #{board [3]} " "|" " #{board [4]} " "|" " #{board [5]} "
+    puts "-----------" 
+    puts " #{board [6]} " "|" " #{board [7]} " "|" " #{board [8]} "
+
+
+
+
+end
+ 
+
+binding.pry
+
+
+
+# board = [" "," "," "," "," "," "," "," "," "] 
+# need to use board array to interpolate positions onto board
+# like board[0] = first cell
